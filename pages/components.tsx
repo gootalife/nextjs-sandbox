df1a1db03d6698851371cab7453f9268f6eb1f56
もにき <gootalife0512@gmail.com>
2022-08-19 01:44:12 +0900

    add treeview

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,25 +1,34 @@
+import { ReactNode, SyntheticEvent, useState } from 'react'
 import Head from 'next/head'
 import { Box, Button, Tab, Tabs, Typography } from '@mui/material'
-import { ReactNode, SyntheticEvent, useState } from 'react'
+import { TreeView, TreeItem } from '@mui/lab'
+import { Info, QuestionAnswer, ExpandMore, ChevronRight } from '@mui/icons-material'
 import { useAlert } from 'hooks/AlertHook'
 import { useConfirm } from 'hooks/ConfirmHook'
-import { Info, QuestionAnswer } from '@mui/icons-material'
 
-interface TabPanelProps {
+interface ITabPanelProps {
   children?: ReactNode
   index: number
   value: number
 }
 
-const TabPanel = (props: TabPanelProps) => {
+interface ITreeViewNode<T> {
+  id: string
+  text: string
+  obj?: T
+  parent?: ITreeViewNode<T>
+  childs: ITreeViewNode<T>[]
+}
+
+const TabPanel = (props: ITabPanelProps) => {
   const { children, value, index, ...other } = props
 
   return (
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
       {...other}
     >
       {value === index && (
@@ -33,8 +42,8 @@ const TabPanel = (props: TabPanelProps) => {
 
 const a11yProps = (index: number) => {
   return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`
+    id: `tab-${index}`,
+    'aria-controls': `tabpanel-${index}`
   }
 }
 
@@ -42,17 +51,81 @@ const Components = () => {
   const [tabNum, setTabNum] = useState(0)
   const [openAlertDialog, renderAlertDialog] = useAlert()
   const [openConfirmDialog, renderConfirmDialog] = useConfirm()
+  const [confirmAnswer, setConfirmAnswer] = useState<boolean>()
+  const nodes: ITreeViewNode<string>[] = [
+    {
+      id: 'obj1',
+      text: 'obj1',
+      childs: [
+        {
+          id: 'obj1-1',
+          text: 'obj1-1',
+          childs: [
+            {
+              id: 'obj1-1-1',
+              text: 'obj1-1-1',
+              childs: []
+            },
+            {
+              id: 'obj1-1-2',
+              text: 'obj1-1-2',
+              childs: []
+            }
+          ]
+        },
+        {
+          id: 'obj1-2',
+          text: 'obj1-2',
+          childs: []
+        }
+      ]
+    },
+    {
+      id: 'obj2',
+      text: 'obj2',
+      childs: [
+        {
+          id: 'obj2-1',
+          text: 'obj2-1',
+          childs: []
+        },
+        {
+          id: 'obj2-2',
+          text: 'obj2-2',
+          childs: [
+            {
+              id: 'obj2-2-1',
+              text: 'obj2-2-1',
+              childs: []
+            }
+          ]
+        }
+      ]
+    }
+  ]
 
+  // タブ選択時
   const handleChangeTabNum = (_event: SyntheticEvent, newTabNum: number) => {
     setTabNum(newTabNum)
   }
+
+  // 再帰でtreeを描画
+  const renderTreeViewFromNodes = <T,>(nodes: ITreeViewNode<T>[]) => (
+    <>
+      {nodes.map((node) => (
+        <TreeItem nodeId={node.id} label={node.text} key={node.id}>
+          {node.childs.length > 0 && <>{renderTreeViewFromNodes(node.childs)}</>}
+        </TreeItem>
+      ))}
+    </>
+  )
 
   return (
     <>
       <Head>
         <title>components</title>
       </Head>
-      <h1>components</h1>
+      <h1>Components</h1>
       <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}>
         <Tabs
           value={tabNum}
@@ -64,12 +137,12 @@ const Components = () => {
           scrollButtons
           allowScrollButtonsMobile
         >
-          <Tab label="Dialogs" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Dialog" {...a11yProps(0)} />
+          <Tab label="Tree View" {...a11yProps(1)} />
           <Tab label="Item Three" {...a11yProps(2)} />
         </Tabs>
         <TabPanel value={tabNum} index={0}>
-          <h1>Dialogs</h1>
+          <h1>Dialog</h1>
           <Box>
             <Button
               sx={{ mr: 1 }}
@@ -87,16 +160,25 @@ const Components = () => {
               sx={{ mr: 1 }}
               variant="contained"
               onClick={async () => {
-                await openConfirmDialog('Comfirm', 'Text')
+                setConfirmAnswer(await openConfirmDialog('Comfirm', 'Are you sure???'))
               }}
               startIcon={<QuestionAnswer />}
             >
               Confirm
             </Button>
+            Answer is: {confirmAnswer === true ? 'YES' : 'NO'}
           </Box>
         </TabPanel>
         <TabPanel value={tabNum} index={1}>
-          Item Two
+          <h1>Tree View</h1>
+          <TreeView
+            aria-label="file system navigator"
+            defaultCollapseIcon={<ExpandMore />}
+            defaultExpandIcon={<ChevronRight />}
+            sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+          >
+            {renderTreeViewFromNodes(nodes)}
+          </TreeView>
         </TabPanel>
         <TabPanel value={tabNum} index={2}>
           Item Three
